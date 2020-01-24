4c0aa419723e98c8a92fc60930c28dc19b97853c
K.Tsukimoto <t.29.wuq@gmail.com>
2020-01-24 22:30:59 +0900

    :100: Registrationのテスト追加

--- server/test/usecase/user_registration_interactor_test.go
+++ server/test/usecase/user_registration_interactor_test.go
@@ -31,12 +31,46 @@ func TestRegistration(t *testing.T) {
 
 	aMock.EXPECT().TemporaryStore(account).Return(err)
 	if (reflect.TypeOf(errors.New("")) != reflect.TypeOf(userRegistrationInteractor.TemporaryRegistration(account, regular))) {
-		t.Errorf("faild: TemporaryRegistration / Expectation: return type err")
+		t.Errorf("faild: TemporaryRegistraton / Expectation: return type err")
 	}
 
 	aMock.EXPECT().TemporaryStore(account).Return(nil)
 	rMock.EXPECT().TemporaryStore(regular).Return(err)
 	if (reflect.TypeOf(errors.New("")) != reflect.TypeOf(userRegistrationInteractor.TemporaryRegistration(account, regular))) {
+		t.Errorf("faild: TemporaryRegistration / Expectation: return type err")
+	}
+
+	aMock.EXPECT().FindByIdFromTemporary("id").Return(account, nil)
+	rMock.EXPECT().FindByIdFromTemporary("id").Return(regular, nil)
+	aMock.EXPECT().Store(account).Return(nil)
+	rMock.EXPECT().Store(regular).Return(nil)
+	if (nil != userRegistrationInteractor.Registration("id")) {
+		t.Errorf("faild: Registration / Expectation: return nil")
+	}
+
+	aMock.EXPECT().FindByIdFromTemporary("id").Return(account, err)
+	if (reflect.TypeOf(errors.New("")) != reflect.TypeOf(userRegistrationInteractor.Registration("id"))) {
+		t.Errorf("faild: TemporaryRegistration / Expectation: return type err")
+	}
+
+	aMock.EXPECT().FindByIdFromTemporary("id").Return(account, nil)
+	rMock.EXPECT().FindByIdFromTemporary("id").Return(regular, err)
+	if (reflect.TypeOf(errors.New("")) != reflect.TypeOf(userRegistrationInteractor.Registration("id"))) {
+		t.Errorf("faild: TemporaryRegistration / Expectation: return type err")
+	}
+
+	aMock.EXPECT().FindByIdFromTemporary("id").Return(account, nil)
+	rMock.EXPECT().FindByIdFromTemporary("id").Return(regular, nil)
+	aMock.EXPECT().Store(account).Return(err)
+	if (reflect.TypeOf(errors.New("")) != reflect.TypeOf(userRegistrationInteractor.Registration("id"))) {
+		t.Errorf("faild: TemporaryRegistration / Expectation: return type err")
+	}
+
+	aMock.EXPECT().FindByIdFromTemporary("id").Return(account, nil)
+	rMock.EXPECT().FindByIdFromTemporary("id").Return(regular, nil)
+	aMock.EXPECT().Store(account).Return(nil)
+	rMock.EXPECT().Store(regular).Return(err)
+	if (reflect.TypeOf(errors.New("")) != reflect.TypeOf(userRegistrationInteractor.Registration("id"))) {
 		t.Errorf("faild: TemporaryRegistration / Expectation: return type err")
 	}
 }
