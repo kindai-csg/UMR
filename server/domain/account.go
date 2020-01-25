b4e089765569f7477147ace6ee5e3e676b7047ff
K.Tsukimoto <t.29.wuq@gmail.com>
2020-01-25 13:43:00 +0900

    :triumph: Accountにvalidate機能の追加

--- server/domain/account.go
+++ server/domain/account.go
@@ -1,11 +1,11 @@
 package domain
 
 type Account struct {
-	ID string
-	Password string
-	Name string
-	EmailAddress string
-	AccountType string
+	ID string `validate:"required,min=4,max=15,alphanumunicode"`
+	Password string `validate:"required,min=8"`
+	Name string `validate:"required"`
+	EmailAddress string `validate:"required,email"`
+	AccountType string `validate:"required,eq=Regular"`
 }
 
 type Accounts []Account
---
+++ server/test/domain/validate_account_test.go
@@ -0,0 +1,114 @@
+package domain_test
+
+import(
+	"github.com/kindaidensan/UMR/domain"
+	"testing"
+	"gopkg.in/go-playground/validator.v9"
+)
+
+func TestAccountValidation(t *testing.T) {
+	validate := validator.New()
+
+	account := &domain.Account{
+		ID: "test",
+		Password: "12345678",
+		Name: "aiueo",
+		EmailAddress: "test@hogehoge.com",
+		AccountType: "Regular",
+	}
+
+	// ---------------------------ID---------------------------
+	account.ID = "t"
+	err := validate.Struct(account)
+	if err == nil {
+		t.Errorf("Faild validation")
+	}
+
+	account.ID = "test123456789012"
+	err = validate.Struct(account)
+	if err == nil {
+		t.Errorf("Faild validation")
+	}
+
+	account.ID = "test@test"
+	err = validate.Struct(account)
+	if err == nil {
+		t.Errorf("Faild validation")
+	}
+
+	account.ID = "test_test1"
+	err = validate.Struct(account)
+	if err == nil {
+		t.Errorf("Faild validation")
+	}
+
+	account.ID = "テスト"
+	err = validate.Struct(account)
+	if err == nil {
+		t.Errorf("Faild validation")
+	}
+
+	account.ID = "four"
+	err = validate.Struct(account)
+	if err != nil {
+		t.Errorf("Faild validation")
+	}
+
+	account.ID = "15mojiIDdasuyo1"
+	err = validate.Struct(account)
+	if err != nil {
+		t.Errorf("Faild validation")
+	}
+	// ---------------------------Password---------------------------
+	account.ID = "test"
+	account.Password = "test"
+	err = validate.Struct(account)
+	if err == nil {
+		t.Errorf("Faild validation")
+	}
+
+	account.Password = "テストだ"
+	err = validate.Struct(account)
+	if err == nil {
+		t.Errorf("Faild validation")
+	}
+
+	account.Password = "テストだテスト"
+	err = validate.Struct(account)
+	if err == nil {
+		t.Errorf("Faild validation")
+	}
+
+	account.Password = "12345678"
+	err = validate.Struct(account)
+	if err != nil {
+		t.Errorf("Faild validation")
+	}
+	// ---------------------------name---------------------------
+	account.Password = "123456789"
+	account.Name = ""
+	err = validate.Struct(account)
+	if err == nil {
+		t.Errorf("Faild validation")
+	}
+	// ---------------------------email---------------------------
+	account.Name = "aiueo"
+	account.EmailAddress = "test@test"
+	err = validate.Struct(account)
+	if err == nil {
+		t.Errorf("Faild validation")
+	}
+
+	account.EmailAddress = "testt.com"
+	err = validate.Struct(account)
+	if err == nil {
+		t.Errorf("Faild validation")
+	}
+	// ---------------------------account type---------------------------
+	account.EmailAddress = "test@hogehoge.com"
+	account.AccountType = "re"
+	err = validate.Struct(account)
+	if err == nil {
+		t.Errorf("Faild validation")
+	}
+}
