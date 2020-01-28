739dd1617e76ca13b305ed4f43be680f500e7c56
K.Tsukimoto <t.29.wuq@gmail.com>
2020-01-28 16:34:21 +0900

    :triumph: validatorの追加

--- server/domain/regular_account.go
+++ server/domain/regular_account.go
@@ -1,18 +1,18 @@
 package domain
 
 type RegularAccount struct {
-	ID string
-	FirstName string
-	LastName string
-	FirstNameKana string
-	LastNameKana string
-	StudentNumber string
-	Department string
-	Grade int
-	PostalCode string
-	StreetAddress string
-	PhoneNumber string
-	EmergencyPhoneNumber string
+	ID string `validate:"required,min=4,max=15,alphanumunicode"`
+	FirstName string `validate:"required"`
+	LastName string `validate:"required"`
+	FirstNameKana string `validate:"required"`
+	LastNameKana string `validate:"required"`
+	StudentNumber string `validate:"required"`
+	Department string `validate:"required"`
+	Grade int `validate:"required,min=1,max=4"`
+	PostalCode string `validate:"required"`
+	StreetAddress string `validate:"required"`
+	PhoneNumber string `validate:"required"`
+	EmergencyPhoneNumber string `validate:"required"`
 }
 
 type RegularAccounts []RegularAccount
