9c0186cd0681c208bfdb4cb15136e5b31a936f0a
K.Tsukimoto <t.29.wuq@gmail.com>
2020-02-15 23:46:46 +0900

    :triumph: LdapHandler Routing

---
+++ server/infrastructure/ldap_handler.go
@@ -0,0 +1,89 @@
+package infrastructure
+
+import (
+	"gopkg.in/ldap.v2"
+	"fmt"
+)
+
+type LdapHandler struct {
+	connection *ldap.Conn 
+}
+
+func NewLdapHandler() *LdapHandler{
+	ldap, err := ldap.Dial("tcp", "ldap:389")
+	if err != nil {
+		return nil 
+	}
+	ldapHandler := LdapHandler {
+		connection: ldap, 
+	}
+	err = ldapHandler.Bind()
+	if err != nil {
+		return nil
+	}
+	return &ldapHandler
+}
+
+func (handler *LdapHandler) Bind() error {
+	err := handler.connection.Bind("cn=Manager,dc=kindai-csg,dc=dev", "densan")
+	if  err != nil {
+		return err
+	}
+	return nil
+}
+
+func (handler *LdapHandler) AddRequest(request []string) error {
+	addRequest := ldap.NewAddRequest(request[1])
+	objectClass := []string{}
+	for i, str := range request {
+		if str == "objectClass" {
+			objectClass = append(objectClass, request[i+1])
+		}
+	}
+	addRequest.Attribute("objectClass", objectClass)
+	for i := 2; i < len(request); i += 2 {
+		if request[i] == "objectClass" {
+			continue
+		}
+		addRequest.Attribute(request[i], []string{ request[i+1] })
+	}
+	err := handler.connection.Add(addRequest)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (handler *LdapHandler) UpdateRequest(id string, request []string) error {
+	return nil
+}
+
+func (handler *LdapHandler) DeleteRequest(id string) error {
+	return nil
+}
+
+func (handler *LdapHandler) SearchRequest(id string) error {
+	searchRequest := ldap.NewSearchRequest(
+		"ou=account,dc=kindai-csg,dc=dev",
+		ldap.ScopeWholeSubtree,
+		ldap.NeverDerefAliases,
+		0,
+		0,
+		false,
+		"(cn="+id+")",
+		[]string{"dn", "cn", "uid", "mail"},
+		nil,
+	)
+	result, err := handler.connection.Search(searchRequest)
+	if err != nil {
+		return err
+	}
+	for _, entry := range result.Entries {
+        fmt.Printf("%s: %v, %v\n", entry.DN, entry.GetAttributeValue("cn"), entry.GetAttributeValue("mail"))
+    }	
+	return nil
+}
+
+func (handler *LdapHandler) Close() {
+	handler.connection.Close()
+}
