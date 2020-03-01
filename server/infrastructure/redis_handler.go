745251bbeb98d702161531b13be31f63d012a3e0
K.Tsukimoto <t.29.wuq@gmail.com>
2020-03-01 18:29:27 +0900

    :heavy_plus_sign: ADD redisにDel MultiDel Incrを追加

--- server/infrastructure/redis_handler.go
+++ server/infrastructure/redis_handler.go
@@ -35,6 +35,32 @@ func (handler *RedisHandler) Get(key string) (string, error) {
 	return value, nil
 }
 
+func (handler *RedisHandler) Del(key string) error {
+	_, err := handler.connection.Do("DEL", key)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (handler *RedisHandler) MultiDel(keys []string) error {
+	err := handler.connection.Send("MULTI")
+	if err != nil {
+		return err
+	}
+	for _, key := range keys {
+		err = handler.connection.Send("DEL", key)
+		if err != nil {
+			return err
+		}
+	}
+	_, err = handler.connection.Do("EXEC")
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
 func (handler *RedisHandler) ExpireSetKey(key string, value string, second int) error {
 	err := handler.connection.Send("MULTI")
 	if err != nil {
@@ -99,4 +125,12 @@ func (handler *RedisHandler) GetKeys(key string) ([]string, error) {
 		result = append(result, str)
 	}
 	return result, nil
+}
+
+func (handler *RedisHandler) Incr(key string) (int, error) {
+	value, err := redis.Int(handler.connection.Do("INCR", key))
+	if err != nil {
+		return -1, err
+	}
+	return value, nil
 }
--- server/interfaces/database/redis_handler.go
+++ server/interfaces/database/redis_handler.go
@@ -3,9 +3,12 @@ package database
 type RedisHandler interface {
 	Set(string, string) (error) 
 	Get(string) (string, error)
+	Del(string) (error)
+	MultiDel([]string) (error)
 	ExpireSetKey(string, string, int) (error)
 	GetTtl(string) (int, error)
 	RPush(string, []string) (error) 
 	LPop(string, int)  ([]string, error)
 	GetKeys(string) ([]string, error)
+	Incr(string) (int, error)
 }
