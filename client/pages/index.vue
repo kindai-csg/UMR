a75686614eed9a9a090c51f41ef38200f5bd30b8
K.Tsukimoto <t.29.wuq@gmail.com>
2020-04-03 22:28:54 +0900

    :heavy_plus_sign: #28 登録後ログインページに飛ぶように

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,92 +1,55 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-flex>
-  </v-layout>
+  <v-card>
+    <v-card-title>Login</v-card-title>
+    <v-card-text>
+       <v-alert type="success" v-if="success">
+        {{ success }}
+      </v-alert>
+      <v-form ref="login_form">
+        <v-text-field
+          v-model="id"
+          label="ID"
+          :rules="[required]"
+        />
+        <v-text-field
+          v-model="password"
+          label="password"
+          type="password"
+          :rules="[required]"
+        />
+      </v-form>
+    </v-card-text>
+    <v-card-actions>
+      <v-btn text v-on:click="login">Login</v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo
+  data() {
+    return {
+      id: "",
+      password: "",
+      required: value => !!value || "必須項目です",
+      success: '',
+    }
+  },
+  mounted() {
+    if (this.$route.query.action) {
+      switch (this.$route.query.action) {
+        case 'register':
+          this.success = '確認メールを送信しました.'
+          break
+      }
+    }
+  },
+  methods: {
+    login() {
+      if (this.$refs.login_form.validate()) {
+        console.log('login')
+      }
+    }
   }
 }
 </script>
