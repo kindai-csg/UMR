0847c7794597372317a6fc4bb8aa778b804d7f22
K.Tsukimoto <t.29.wuq@gmail.com>
2020-04-28 00:14:22 +0900

    :art: ダークテーマ取りやめ

--- client/nuxt.config.js
+++ client/nuxt.config.js
@@ -72,7 +72,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
